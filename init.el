f3b47ae195492341585b57f93b1ddcdfd459fe54
Thor <thorck@protonmail.com>
2021-12-13 22:07:17 +0000

    pass

--- init.el
+++ init.el
@@ -97,7 +97,7 @@
        lsp               ; M-x vscode
        (magit +forge)             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
-       (pass +auth-source-pass)              ; password manager for nerds
+       (pass +auth)              ; password manager for nerds
        pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
        ;;rgb               ; creating color strings
