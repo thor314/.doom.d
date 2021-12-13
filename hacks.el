4457afd26eaac45725488f95c8bb9c0fa6f97a6b
Thor <thorck@protonmail.com>
2021-12-13 16:53:32 +0000

    snippets comin back

---
+++ hacks.el
@@ -0,0 +1,20 @@
+;;; hacks.el -*- lexical-binding: t; -*-
+;;; mah hacky lil frens
+
+(defun tk/split-window-horizontally ()
+  (interactive)
+  (split-window-horizontally)
+  (consult-buffer))
+
+(defun insert-date (prefix)
+  "Insert the current date. With prefix-argument, use ISO format. With
+  two prefix arguments, write out the day and month name."
+  (interactive "p")
+  (let ((format
+          (cond
+            ((not prefix) "%Y-%m-%d")
+            ((equal prefix '(4)) "%A, %d %B %Y")))
+         (system-time-locale "us_US"))
+    (insert (format-time-string format))))
+
+;; (insert-date 4)
--- packages.el
+++ packages.el
@@ -4,11 +4,13 @@
 ;; To install a package with Doom you must declare them here and run 'doom sync'
 ;; on the command line, then restart Emacs for the changes to take effect -- or
 ;; use 'M-x doom/reload'.
+
+;;(package! yasnippet-snippets)
 (package! git-link)
 (use-package! git-link :commands git-link)
 (package! super-save)
 (package! tide) ;; typescript
-
+(package! telega)
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 ;(package! some-package)
 
